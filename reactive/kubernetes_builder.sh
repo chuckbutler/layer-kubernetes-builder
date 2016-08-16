562ca33c2a329b122cbfc3c54d90823cbdb6c7c6
Charles Butler <chuck@dasroot.net>
2016-08-16 11:17:03 -0500

    Adds a bare-bones readme, cleans up reactive module

--- reactive/kubernetes_builder.sh
+++ reactive/kubernetes_builder.sh
@@ -6,24 +6,24 @@ source charms.reactive.sh
 
 @when_not 'docker.configured'
 function install_kubernetes-builder() {
-usermod -G docker jenkins
+  usermod -G docker jenkins
 
-# Reconfigure docker to use AUFS instead of DeviceMapper
-apt update -y
-apt install -y aufs-tools \
-               linux-image-extra-`uname -r` \
-               git
+  # Reconfigure docker to use AUFS instead of DeviceMapper
+  apt update -y
+  apt install -y aufs-tools \
+                 linux-image-extra-`uname -r` \
+                 git
 
-cat << EOF > /etc/default/docker.io
+  cat << EOF > /etc/default/docker.io
 # THIS FILE IS MANAGED BY A BUILD SCRIPT!
 DOCKER_OPTS="--storage-driver=aufs"
 EOF
 
 
-# restart jenkins so it can access docker
-service jenkins restart
+  # restart jenkins so it can access docker
+  service jenkins restart
 
-charms.reactive set_state 'docker.configured'
+  charms.reactive set_state 'docker.configured'
 }
 
 
